4acac5e09fa00c4403d9cd9d51dfbdaa7cf9b1a6
akhilveeraghanta <31548846+akhilveeraghanta@users.noreply.github.com>
2017-10-05 18:15:52 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,23 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+
+url1 = "https://www.thriftstore.ca/british-columbia/drop-bin-locations"
+r1 = requests.get(url1)
+
+soup1 = BeautifulSoup(r1.content,"lxml")
+soup1.prettify();
+
+filtered1 = soup1.find_all("tr")
+
+
+'''prints out all the addresses stored
+in the table on URL1'''
+
+for sub1 in filtered1:
+    inter = sub1.find_all("span")
+    for sub2 in inter:
+        if(len(sub2.text)>2):
+            print(sub2.text)
+    print("------------")
 
